d8def37522665342dfa857ae325b012aac977bdf
sarojbhattarai <sarojbhattarai800@gmail.com>
2019-01-28 22:48:43 +0545

    user form practice

--- mysite/main/urls.py
+++ mysite/main/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
 
+app_name = "main"
+
 urlpatterns = [
-    path('', views.index, name = "Index"),
+    path('', views.index, name = "index"),
+    path('register/', views.register, name = "register"),
 ]
